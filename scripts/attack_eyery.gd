2c7130bad8943f2e386a7886d710eb4fc7ab05ee
Koidnulu <sunlanxi2008@gmail.com>
2026-08-01 16:41:58 -0700

    new updates

--- scripts/attack_eyery.gd
+++ scripts/attack_eyery.gd
@@ -6,12 +6,13 @@ var boss: CharacterBody2D
 var character: AnimatedSprite2D
 @onready var player := $"../../../main_character"
 var is_attacking := false;
+@onready var flipping: Node2D = $"../../flipping"
 
 func enter() -> void:
 	# does not hard code names bcs it just grabs the top as boss, find child of
 	# boss that is animated sprite 2d which corresponds to character
 	boss = owner as CharacterBody2D
-	character = boss.get_node("AnimatedSprite2D") as AnimatedSprite2D
+	character = boss.get_node("flipping/AnimatedSprite2D") as AnimatedSprite2D
 	while(true):
 		is_attacking = true
 		character.play("hurt")
@@ -23,11 +24,8 @@ func physics_update(delta: float):
 	var distance = (player.global_position - boss.global_position).length()
 	var direction = (player.global_position - boss.global_position).normalized()
 	if direction.x > 0:
-		character.flip_h = false;
-		character.offset.x = 40
+		flipping.scale.x = 1;
 	elif direction.x < 0:
-		character.flip_h = true;
-		character.offset.x = -40
+		flipping.scale.x = -1;
 	if distance > 80 and get_tree().current_scene.name  == "battle eyery":
 		state_machine.change_state("follow_eyery")
-	
--- scripts/idle_eyery.gd
+++ scripts/idle_eyery.gd
@@ -9,7 +9,7 @@ func enter() -> void:
 	# does not hard code names bcs it just grabs the top as boss, find child of
 	# boss that is animated sprite 2d which corresponds to character
 	boss = owner as CharacterBody2D
-	character = boss.get_node("AnimatedSprite2D") as AnimatedSprite2D
+	character = boss.get_node("flipping/AnimatedSprite2D") as AnimatedSprite2D
 	character.play("idle")
 	
 func physics_update(delta: float) -> void:
